287e04adadec3fa32b1af9b25a501d3e04257c73
mandy abade <20241ctb0100049@estudantes.ifpr.edu.br>
2026-06-23 20:27:08 -0300

    aula02-metade

--- app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
+++ app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
@@ -1,16 +1,15 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
- * most up to date changes to the libraries and their usages.
- */
-
 package com.ifpr.wearostemplate.presentation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ifpr.wearostemplate.presentation.PerfilActivity
 import com.ifpr.wearostemplate.R
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -19,6 +18,11 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContentView(R.layout.activity_main)
+
+        val btnPerfil = findViewById<Button>(R.id.btnPerfil)
+
+        btnPerfil.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PerfilActivity::class.java))
+        }
     }
 }
-
---
+++ app/src/main/java/com/ifpr/wearostemplate/presentation/PerfilActivity.kt
@@ -0,0 +1,13 @@
+package com.ifpr.wearostemplate.presentation
+
+import android.os.Bundle
+import android.app.Activity
+import com.ifpr.wearostemplate.R
+
+class PerfilActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
